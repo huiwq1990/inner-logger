d0dec997a5e4fff5b2e5a2d897c25f494fb9efc7
qihao <qihao@taobao.com>
2015-02-06 16:18:56 +0800

    修改更新目录

--- src/main/java/com/alibaba/middleware/innerlog/LoggerClassLoader.java
+++ src/main/java/com/alibaba/middleware/innerlog/LoggerClassLoader.java
@@ -32,7 +32,7 @@ public class LoggerClassLoader extends ClassLoader {
 	/**
 	 * 将JAR内的innerLib导出到文件系统需要的常量
 	 */
-	private final static String SYSTEM_TMP_DIR = System.getProperty("user.dir");
+	private final static String SYSTEM_TMP_DIR = System.getProperty("user.home");
 	private final static String SYSTEM_FILE_SEP = System.getProperty("file.separator");
 	private final static String OUT_LIB_DIR_NAME = ".inner-logger";
 	private final static String OUT_LIB_DIR_PATH = SYSTEM_TMP_DIR+ SYSTEM_FILE_SEP + OUT_LIB_DIR_NAME;
